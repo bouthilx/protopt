254f1c7df669ae672c3378fba4d109328c50ef24
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-01-26 20:18:35 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Installation script."""
+from os import path
+from io import open
+from setuptools import setup
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read().strip()
+
+setup(
+    name='protopt',
+    description='',
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/bouthilx/protopt.git',
+    author='Xavier Bouthillier',
+    license='MIT',
+    packages=['protopt'],
+    install_requires=['pymongo', 'sacred',  # (my branch)
+                      'gitpython'],
+    scripts=['bin/opt-launch',
+             'bin/opt-run'],
+    #         'bin/opt-stats']
+    # Optional: pymongo, sacred (my branch), gitpython
+)
