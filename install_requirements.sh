aa4e20c0de8127d3251891feb68fabf56bca01b9
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-02-27 22:07:35 -0800

    Fix install
    Why:
    
    Numpy version was changed in cluster and needed local upgrade. Seems
    to...

--- install_requirements.sh
+++ install_requirements.sh
@@ -7,12 +7,13 @@ WHEEL_DIR=$HOME/wheels/$PROJECT
 
 echo "debug: install from wheels"
 pip install --use-wheel --no-index --find-links=$WHEEL_DIR subprocess32
-pip install --use-wheel --no-index --find-links=$WHEEL_DIR smart_dispatch
 pip install --use-wheel --no-index --find-links=$WHEEL_DIR pymongo gitpython
+pip install --use-wheel --no-index --find-links=$WHEEL_DIR -U numpy scipy scikit-optimize
+# Do not upgrade, use the wheel directly
+pip install --use-wheel --no-index --find-links=$WHEEL_DIR smart_dispatch
 pip install --use-wheel --no-index --find-links=$WHEEL_DIR sacred 
-pip install --use-wheel --no-index --find-links=$WHEEL_DIR numpy scipy scikit-optimize
 
-if [ ${PROJECT_PATH} -z ]
+if [ ! -z "${PROJECT_PATH}" ]
 then
     bash ${PROJECT_PATH}/install_requirements.sh
 fi
